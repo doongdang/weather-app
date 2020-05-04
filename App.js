a6504d1e79d2b29c8a7e8e41ef13eb2fbefd760b
SeokHyun Cho <doongdang@naver.com>
2020-05-04 16:18:04 +0900

    Permission Location

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import Loading from "./Loading";
+import { Alert } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const [isLoading, setIsLoading] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function getLocation() {
+      try {
+        await Location.requestPermissionsAsync();
+        const {
+          coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync();
+        setIsLoading(false);
+      } catch (error) {
+        Alert.alert("Can`t find", "Try Again");
+      }
+    }
+    getLocation();
+  }, []);
+
+  return isLoading ? <Loading /> : null;
+}
